d1ec66b4c35e095e10995cd70f28a35561e1b7cc
joaopedro5g <ebtjoaopedro@gmail.com>
2025-04-27 13:25:33 -0300

    Created read file function in Rust

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import * as pathfs from "node:path";
 type User = {
   id: string;
   username: string;
@@ -7,11 +8,13 @@ type User = {
   country: string;
 };
 
+const dylib = Deno.dlopen("./libread_bigdata.so", {
+  user_has_admin: { parameters: ["buffer", "usize"], result: "pointer" },
+  read_file: { parameters: ["buffer"], result: "pointer" },
+});
+
 (async () => {
   const startTimestamp = Date.now();
-  const dylib = Deno.dlopen("libread_bigdata.so", {
-    user_has_admin: { parameters: ["buffer", "usize"], result: "pointer" },
-  });
 
   const reader = await Deno.readTextFile("./data.json");
   const encoder = new TextEncoder();
@@ -20,13 +23,38 @@ type User = {
   const resultPtr = dylib.symbols.user_has_admin(data, BigInt(data.length));
   if (resultPtr) {
     const view = new Deno.UnsafePointerView(resultPtr);
-    const text = JSON.parse(view.getCString());
-    console.log(text);
+    const text: User[] = JSON.parse(view.getCString());
+    console.log(`Encontrados ${text.length} usuarios correspondentes`);
   } else {
     console.error("Erro, ponteiro nullo");
   }
   const finalTimestamp = Date.now();
   console.log(
-    `${finalTimestamp - startTimestamp}ms foi para gerar tudo isso aí`,
+    `${finalTimestamp - startTimestamp}ms de resposta de processamento`,
+  );
+});
+
+(() => {
+  const startTimestamp = Date.now();
+  const encoder = new TextEncoder();
+  const path = encoder.encode("./data.json" + "\0");
+  const resultPtrRF = dylib.symbols.read_file(path);
+  if (resultPtrRF) {
+    const view = new Deno.UnsafePointerView(resultPtrRF);
+    const encoder = new TextEncoder();
+    const dataptr = encoder.encode(view.getCString());
+    const resultPtrUHA = dylib.symbols.user_has_admin(
+      dataptr,
+      BigInt(dataptr.length),
+    );
+    if (resultPtrUHA) {
+      const view = new Deno.UnsafePointerView(resultPtrUHA);
+      const text: User[] = JSON.parse(view.getCString());
+      console.log(`Encontrados ${text.length} usuarios correspondentes`);
+    }
+  }
+  const finalTimestamp = Date.now();
+  console.log(
+    `${finalTimestamp - startTimestamp}ms de resposta de processamento`,
   );
 })();
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use std::ffi::{CStr, CString};
+use std::fs;
+use std::os::raw::c_char;
 use std::thread;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct User {
@@ -9,7 +12,7 @@ pub struct User {
     email: String,
     gender: String,
     superadmin: bool,
-    country: String
+    country: String,
 }
 
 #[unsafe(no_mangle)]
@@ -28,14 +31,11 @@ pub extern "C" fn user_has_admin(data: *const u8, data_len: usize) -> *const u8 
     for chunk in users.chunks(chunk_size) {
         let local_chunk = chunk.to_vec();
         let th = thread::spawn(move || {
-            let superadmins = local_chunk
-                .into_iter()
-                .filter(|u| u.superadmin).map(|u| u);
+            let superadmins = local_chunk.into_iter().filter(|u| u.superadmin).map(|u| u);
             superadmins
         });
         handles.push(th);
     }
-
 
     let mut total_superadmins = Vec::new();
 
@@ -49,6 +49,23 @@ pub extern "C" fn user_has_admin(data: *const u8, data_len: usize) -> *const u8 
     ptr
 }
 
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn read_file(directory: *const c_char) -> *const c_char {
+    let c_str = unsafe { CStr::from_ptr(directory) };
+    let path = match c_str.to_str() {
+        Ok(s) => s,
+        Err(_) => return std::ptr::null(),
+    };
+    let content = match fs::read_to_string(path) {
+        Ok(result) => result,
+        Err(_) => return std::ptr::null(),
+    };
+    let c_content = match CString::new(content) {
+        Ok(c) => c,
+        Err(_) => return std::ptr::null(),
+    };
+    c_content.into_raw()
+}
 
 // #[unsafe(no_mangle)]
 // pub extern "C" fn free_user_has_admin(ptr: *mut u8, len: usize) {
